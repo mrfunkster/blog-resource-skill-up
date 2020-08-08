b4a1dce047af992208617567ca88b17b4ef83d6c
mr.funkster <mr.funksters@gmail.com>
2020-08-09 00:17:29 +0300

    Added body overlay and body lock class

--- script/burger.js
+++ script/burger.js
@@ -1,14 +1,18 @@
 let burgerBtn = document.querySelector('.mobile-menu');
 let navMenu   = document.querySelector('.nav-menu');
+let overlay   = document.querySelector('.overlay');
 let main      = document.querySelector('.main');
+let topMenu   = document.querySelector('.top-menu')
 
 burgerBtn.addEventListener('click', function() {
     burgerBtn.classList.toggle('active');
     if (burgerBtn.classList.contains('active')) {
         navMenu.classList.add('active');
+        overlay.classList.add('active');
         document.body.classList.add('lock');
     } else {
         navMenu.classList.remove('active');
+        overlay.classList.remove('active');
         document.body.classList.remove('lock');
     }
 })
@@ -16,11 +20,18 @@ burgerBtn.addEventListener('click', function() {
 function closeBurger() {
     navMenu.classList.remove('active');
     burgerBtn.classList.remove('active');
+    overlay.classList.remove('active');
     document.body.classList.remove('lock');
 }
 
 main.addEventListener('click', function(e) {
     if (e.target != main) {
+        closeBurger();
+    }
+})
+
+topMenu.addEventListener('click', function(e) {
+    if (e.target != topMenu) {
         closeBurger();
     }
 })
